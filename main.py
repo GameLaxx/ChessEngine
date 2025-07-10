00581c975ca2278d45f3e3f0c12360ee23aaeeed
GameLaxx <tmboisse@gmail.com>
2025-07-10 17:13:38 +0200

    End of v1

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from chess_game import ChessGame
+from chess_render import ChessRender
 from chess_bot import ChessBot, ChessParams
 
 board = ChessGame()
-bot1_params = ChessParams(king_safety=0.2, pawn_center=0.5, square_attacked=0.2)
-bot2_params = ChessParams()
+bot1_params = ChessParams()
+bot2_params = ChessParams().random()
 bot1 = ChessBot(bot1_params)
 bot2 = ChessBot(bot2_params)
-
-while board.winner == -1:
-    to_play = None
-    if board.player_turn == board.WHITE:
-        to_play = bot1.make_decision(board)
-    else:
-        to_play = bot2.make_decision(board)
-    board.play(to_play)
-if board.winner == 0:
-    print("White won !")
-elif board.winner == 1:
-    print("Black won !")
-elif board.winner == 2:
-    print("Draw !")
+render = ChessRender(board, opponent2=bot2)
+render.update()
