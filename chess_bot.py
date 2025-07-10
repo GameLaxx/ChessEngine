930b40df3d2abf1476122d0a6e1b6ea01ce90e78
GameLaxx <tmboisse@gmail.com>
2025-07-10 15:32:49 +0200

    Documentation bot

--- chess_bot.py
+++ chess_bot.py
@@ -30,7 +30,11 @@ class ChessParams():
         }
         self.score = 0
     
-    def random(self):
+    def random(self) -> 'ChessParams':
+        """
+        Returns:
+            (ChessParams) : Create a random set of parameters.
+        """
         for key in self.dict:
             if key == "check_mate":
                 continue
@@ -47,7 +51,11 @@ class ChessBot():
         self.center_squares = 1 << 27 | 1 << 28 | 1 << 35 | 1 << 36
         self.debug = debug
 
-    def __rule_material(self, bitboards, occupancy):
+    def __rule_material(self) -> float:
+        """
+        Returns:
+            (float) : The difference of material in the position.
+        """
         ret = 0
         for piece in range(5):
             piece_white = self.current_board.count_piece_wholeboard(self.WHITE, piece)
@@ -55,9 +63,13 @@ class ChessBot():
             ret += self.params[self.values[piece]] * (piece_white - piece_black)
         return ret
     
-    def __rule_pawnspace(self, bitboards, occupancy):
-        white_pawn_squares = self.current_board.bitboard_to_squares(bitboards[self.WHITE][0])
-        black_pawn_squares = self.current_board.bitboard_to_squares(bitboards[self.BLACK][0])
+    def __rule_pawnspace(self) -> float:
+        """
+        Returns:
+            (float) : The difference of space behind the pawns.
+        """
+        white_pawn_squares = self.current_board.bitboard_to_squares(self.current_board.bitboards[self.WHITE][0])
+        black_pawn_squares = self.current_board.bitboard_to_squares(self.current_board.bitboards[self.BLACK][0])
         white_space = 0
         black_space = 0
         for row, _ in white_pawn_squares:
@@ -66,26 +78,38 @@ class ChessBot():
             black_space += row
         return self.params["pawn_space"] * (white_space - black_space)
     
-    def __rule_pawncenter(self, bitboards, occupancy):
-        center_white_bb = bitboards[self.WHITE][0] & self.center_squares
-        center_black_bb = bitboards[self.BLACK][0] & self.center_squares
+    def __rule_pawncenter(self) -> float:
+        """
+        Returns:
+            (float) : The difference between the number of pawns in the center.
+        """
+        center_white_bb = self.current_board.bitboards[self.WHITE][0] & self.center_squares
+        center_black_bb = self.current_board.bitboards[self.BLACK][0] & self.center_squares
         nb_center_pawns_white = self.current_board.count_piece_bitboard(center_white_bb)
         nb_center_pawns_black = self.current_board.count_piece_bitboard(center_black_bb)
         return self.params["pawn_center"] * (nb_center_pawns_white - nb_center_pawns_black)
     
-    def __rule_squareattacked(self, bitboards, occupancy):
+    def __rule_squareattacked(self) -> float:
+        """
+        Returns:
+            (float) : The difference between the number of square attacked.
+        """
         white_attacks = self.current_board.board_attacked(self.BLACK)
         black_attacks = self.current_board.board_attacked(self.WHITE)
         nb_attacks_white = self.current_board.count_piece_bitboard(white_attacks)
         nb_attacks_black = self.current_board.count_piece_bitboard(black_attacks)
         return self.params["square_attacked"] * (nb_attacks_white - nb_attacks_black)
     
-    def __rule_kingsafety(self, bitboards, occupancy):
+    def __rule_kingsafety(self) -> float:
+        """
+        Returns:
+            (float) : The difference between the number of square attacked around the king.
+        """
         white_attacks = self.current_board.board_attacked(self.BLACK)
         black_attacks = self.current_board.board_attacked(self.WHITE)
         count = 0
         for color in [self.WHITE,self.BLACK]:
-            king_index = bitboards[color][self.current_board.KING].bit_length() - 1
+            king_index = self.current_board.bitboards[color][self.current_board.KING].bit_length() - 1
             king_rank, king_file = divmod(king_index, 8)
             deltas = self.current_board.KING_DELTAS
             for delta in deltas:
@@ -101,7 +125,11 @@ class ChessBot():
                     count += 1 if white_attacks & 1 << neighbor_index else 0
         return self.params["king_safety"] * count
     
-    def __rule_bishoppair(self, bitboards, occupancy):
+    def __rule_bishoppair(self) -> float:
+        """
+        Returns:
+            (float) : Rewards the side with the bishop pair.
+        """
         bishop_white = self.current_board.count_piece_wholeboard(self.WHITE, self.current_board.BISHOP)
         bishop_black = self.current_board.count_piece_wholeboard(self.BLACK, self.current_board.BISHOP)
         if bishop_black == bishop_white:
@@ -112,20 +140,28 @@ class ChessBot():
             return -self.params["bishop_pair"]
         return 0
     
-    def assign_score(self, bitboards, occupancy):
+    def assign_score(self) -> float:
+        """
+        Returns:
+            (float) : Go through every rule and add their result to assing a score to a position.
+        """
         ret = 0
         for name in dir(self):
             if name.startswith("_ChessBot__rule_"):
                 func = getattr(self, name)
                 if callable(func):
-                    ret += func(bitboards, occupancy)
+                    ret += func()
         return ret
     
-    def evaluate(self):
+    def evaluate(self) -> float:
+        """
+        Returns:
+            (float) : For a position, go through all the responses of the opponent to give the score for the best response.
+        """
         ret = None
-        for move in self.current_board.get_moves(self.current_board.player_turn):
+        for move in self.current_board.get_moves(self.current_board.player_turn): # same behaviour as make_decision method
             self.current_board._move(move)
-            score = self.assign_score(self.current_board.bitboards, self.current_board.occupancy)
+            score = self.assign_score()
             if ret == None:
                 ret = score
                 self.current_board._pop()
@@ -147,32 +183,36 @@ class ChessBot():
                 ret = 0
         return ret
 
-    def make_decision(self, board : ChessGame):
-        self.current_board = board
-        moves = self.current_board.get_moves(self.current_board.player_turn)
+    def make_decision(self, board : ChessGame) -> str:
+        """
+        Returns:
+            (str) : Go through all the moves and assign a score for each. Then send the best move to play.
+        """
+        self.current_board = board # store the board to avoid passing it to every function
+        moves = self.current_board.get_moves(self.current_board.player_turn) # get all the moves for the current position
         max_score = None
-        to_play = []
+        to_play = [] # we randomly chose the best move from a list of same score moves
         for move in moves:
-            self.current_board._move(move)
-            player_attacked = (self.current_board.player_turn + 1) % 2
+            self.current_board._move(move) # push the current position before moving, pop to return to position and player
+            player_attacked = (self.current_board.player_turn + 1) % 2 # manually change player
             self.current_board.player_turn = player_attacked
             score = self.evaluate()
-            if max_score == None:
+            if max_score == None: # always store the score if none was added before
                 max_score = score
                 to_play.append(move)
                 self.current_board._pop()
                 continue
-            if score == max_score:
+            if score == max_score: # if same score then add to the list
                 to_play.append(move)
                 self.current_board._pop()
                 continue
-            if player_attacked == self.BLACK:
+            if player_attacked == self.BLACK: # if black then add only if the score is worse
                 if max_score < score:
                     max_score = score
                     to_play = [move]
                 self.current_board._pop()
                 continue
-            if max_score > score:
+            if max_score > score: # if white the add only if the score is better
                 max_score = score
                 to_play = [move]
             self.current_board._pop()
